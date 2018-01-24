da308e12fb486ec4fc5ffa2ddcf90af66eb1d76b
jihed <jihedbibani27@gmail.com>
2018-01-24 10:26:40 +0100

    binding two ways

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {FormsModule} from "@angular/forms";
 
 import { AppComponent } from './app.component';
 import { Comp1Component } from './comp1/comp1.component';
 import { Comp2Component } from './comp2/comp2.component';
 import { ColorsComponent } from './colors/colors.component';
+import { TwowayComponent } from './twoway/twoway.component';
 
 
 @NgModule({
@@ -13,10 +14,12 @@ import { ColorsComponent } from './colors/colors.component';
     AppComponent,
     Comp1Component,
     Comp2Component,
-    ColorsComponent
+    ColorsComponent,
+    TwowayComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
